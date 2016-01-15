1749a55ae9750e2d40bf60fa163c9ce88c54ef25
Szymon Nowicki <szymon.nowicki@agitive.com>
2016-01-15 12:37:42 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  render() {
+    return ( <div>
+      <h1>Hello, world.</h1>
+      <p> My first awesome app!</p>
+        <p>It is sooo awesome !!!</p>
+      </div>
+    );
+  }
+}
