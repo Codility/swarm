aa03fd936568bd2d4527f431c5d73427c4d3a35e
Szymon Nowicki <szymonnowicki@gmail.com>
2016-06-03 11:28:17 +0200

    Add React Redux

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import App from './App';
+import swarmReducer from './reducers';
 
 require('./style.css');
 
-ReactDOM.render(<App />, document.getElementById('root'));
+let store = createStore(swarmReducer);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App/>
+  </Provider>,
+  document.getElementById('root'));
---
+++ src/reducers.js
@@ -0,0 +1,7 @@
+
+
+const swarmReducer = (state = {}, action) => {
+  return state;
+};
+
+export default swarmReducer
